b13d6f2dcfd6dc939af2693240d6a1cd06915dc1
Frans Korhonen <frans.korhonen@helsinki.fi>
2009-01-20 16:04:57 +0000

    *** empty log message ***

--- tupa2/views.py
+++ tupa2/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.http import HttpResponse
+# coding: latin-1
+
+def index(request):
+
+    return HttpResponse("<html><p><A href='./admin/'>AdminSivu.</A><p><h1>Tähän tulee Tupan testisivustot...</h1><p></html>")
+
