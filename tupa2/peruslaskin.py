a637f58b7283147e3da0a224633aa41bc75dd3cd
Frans Korhonen <frans.korhonen@helsinki.fi>
2009-01-25 15:33:29 +0000

    Peruslaskin moduuli. Laskee merkkijonoja.

---
+++ tupa2/peruslaskin.py
@@ -0,0 +1,79 @@
+# coding: latin-1
+from decimal import *
+import re
+
+def operoi(alku,operaattori,loppu) :
+    """
+    Poimii desimaaliluvun alku stringin lopusta, sekä loppu stringin alusta, 
+    Suorittaa niille valitun lasku operaation. 
+    Palauttaa merkkijonon jossa numerot ja operaattori on korvattu kyseisen operaation tuloksella.
+    """ 
+    lauseke="str( Decimal('" + re.search( r'^\-\d\.\d*\Z|\d\.\d*\Z|\d*\Z|\d\Z ',alku).group(0)+ "')" 
+    lauseke=lauseke + operaattori 
+    lauseke=lauseke +"Decimal('" + re.search(r'\A\-\d*\.\d*|\A\d*\.\d*|\A\d*|\A\d',loppu).group(0) + "'))"
+    tulos = eval(lauseke)
+    lauseke=  re.sub(r'^\-\d\.\d*\Z|\d\.\d*\Z|\d*\Z|\d\Z',"", alku) + tulos 
+    lauseke=lauseke + re.sub(r'\A\-\d*\.\d*|\A\d*\.\d*|\A\d*|\A\d',"", loppu)
+    return lauseke    
+
+def suorita(kaava) :
+    """
+    Suorittaa merkkijonon laskutoimituksen jossa ei ole sulkeita() 
+    Laskee ensin kerto- sekä jakolaskun * /
+    Sitten plus ja miinus. + -
+    Operaattoreiden välissä voi olla vain lukuja (-)(0-9)(.)(0-9)
+    Palauttaa tuloksen merkkijonona.
+    """
+    laskettu=kaava
+    operaattorit=( ("*","/") , ("+","-")  )
+    haut = (r"\*|/",r"\+|\-")
+    for r in range(len(haut)) :
+        for o in operaattorit[r]  :
+            haku=re.search( haut[r] , laskettu[1:] )
+            if haku:
+                i=haku.start()+1
+                if laskettu[i] == o :
+                        laskettu= operoi(laskettu[:i], o, laskettu[i+1:])
+    
+    if re.search(r'\*|/|\+|\-',laskettu[1:]) :
+        return suorita(laskettu)   
+    else:
+        return laskettu
+
+def validioi(kaava) :
+     """
+     Tarkistaa kaavan laskettavuuden:
+     -Tarkistaa että kaava sisältää vain sallittuja merkkejä
+     -Myöhemmin myös että sulut ovat oikein ja että operaattoreiden välissä on luku.
+     Palauttaa True/False
+     """
+     muokattu= re.sub('\d*\.?\d*',"", kaava)
+     muokattu= re.sub('\+|\-|\*|/|\(|\)| ',"", muokattu)
+     if len(muokattu)==0: 
+         return True
+     else :
+         return False 
+
+def laske(kaava,sanakirja=None) :
+    """
+    Laskee kaavan sulkujen kanssa rekursiivisesti.
+    Suortittaa +-*/ toimitukset.
+    Palauttaa tuloksen jos kaava on laskettavissa. 
+    Muussa tapauksessa palautusarvo on None.
+    """
+    muokattu=kaava
+    if validioi(muokattu)==False :
+        return None
+    sulku=re.search( r"\(" , muokattu)
+    if sulku :
+        i=sulku.start()
+        muokattu=muokattu[:i]+laske(muokattu[i+1:])
+    sulku=re.search( r"\)" , muokattu)
+    if sulku :
+        i=sulku.start()
+        muokattu=suorita(muokattu[:i])+muokattu[i+1:]
+    if not re.search( r"\)" , muokattu):
+        if not re.search( r"\(", muokattu):
+             muokattu=suorita(muokattu)
+    return muokattu
+
