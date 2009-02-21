6729b9d4123c1352fcf84a31195f56a3a4538b96
Samu Wikstedt <swiksted@gmail.com>
2009-02-21 17:45:55 +0000

    Lisätty pari peruslaskuntestiä, joista pitäisi tulla None tulos, kuten tyhjä syöte ja ei-numeerinen syöte.

--- web/tupa/tests.py
+++ web/tupa/tests.py
@@ -11,31 +11,37 @@ class peruslaskin_test(unittest.TestCase):
 
     """
     def testYhteenlasku(self):
-        assert  laske('5+5') == '10'
+        assert    laske('5+5') == '10'
     
     def testYhteenlasku_spacet(self):
-        assert  laske(' 5 + 5 ') == '10'
+        assert    laske(' 5 + 5 ') == '10'
     
     def testDesimaaliluku(self):
-        assert  laske('0.5*2+10.5000') == '11.5'
+        assert    laske('0.5*2+10.5000') == '11.5'
     
     def testMuuttujavirhe(self):
-        assert  laske('tyhja_muuttuja*5') == None
+        assert    laske('tyhja_muuttuja*5') == None
 
     def testSulkulauseke(self):
-        assert  laske('2*(1+9)') == '20'
+        assert    laske('2*(1+9)') == '20'
 
     def testSulut_sisakkain(self):
-        assert  laske('10*((1+2)+(5*10))') == '530'
+        assert    laske('10*((1+2)+(5*10))') == '530'
 
     def testSulut_vaarinpain(self):
-        assert  laske('10*)(1+2)+(5*10))') == None
+        assert    laske('10*)(1+2)+(5*10))') == None
 
     def testSulut_vaaramaara(self):
-        assert  laske('10*(1+2)+(5*10))') == None
+        assert    laske('10*(1+2)+(5*10))') == None
 
     def testNollallajako(self):
-        assert  laske('10/0') == None 
+        assert    laske('10/0') == None
+        
+    def testTyhjasyote(self):
+        assert    laske('') == None
+        
+    def testEiaritmtetiikkaa(self):
+        assert    laske('a+ b-c*d') == None
 
      
 class lasketulos_test(unittest.TestCase):
