2954eac8f13b70591e7a53505e4f2da159cdfcdc
Frans Korhonen <frans.korhonen@helsinki.fi>
2009-02-22 07:39:21 +0000

    *** empty log message ***

--- web/tupa/views.py
+++ web/tupa/views.py
@@ -5,53 +5,42 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 import operator
 import decimal 
 from django.newforms import *
-from django.forms import *
+import django.forms
 
 def index(request):
       kisat = Kisa.objects.all()
       return render_to_response('tupa/index.html', {'latest_kisa_list': kisat })
 
-def kisa(request,kisa_id):
-      sarjat = Sarja.objects.filter(kisa__id=kisa_id)
+def kisa(request,kisa_nimi):
+      sarjat = Sarja.objects.filter(kisa__nimi=kisa_nimi)
       return render_to_response('tupa/kisa.html', {'sarja_list': sarjat })
+
+def maaritaKisa(request, kisa_nimi):
+      return HttpResponse("KISAN " + kisan_nimi + " MÄÄRITYS"  )
+
+def maaritaTehtava(request, kisa_nimi, tehtava_id):
+      return HttpResponse(kisan_nimi + " TEHTAVAN " + tehtava_id + " MÄÄRITYS")
+
+def syotaKisa(request, kisa_nimi):
+      return HttpResponse("KISAN " + kisa_nimi + " SYÖTTÖ")
+
+def syotaTehtava(request, kisa_nimi , tehtava_id) :
+      return HttpResponse( kisa_nimi + " TEHTÄVÄN " + tehtava_id + " SYÖTTÖ")
+
+def tulostaSarja(request, kisa_nimi, sarja_id) :
+      sarja = Sarja.objects.filter(id=sarja_id)[0]
+      tulokset= sarja.laskeTulokset()
+      return render_to_response('tupa/tulokset2.html', {'tulos_taulukko' : tulokset }  )
 
 def sarja(request,sarja_id) :
       sarja= Kisa.objects.filter(id=sarja_id)[0]
       return render_to_response('tupa/sarja.html', {'sarja_object': sarja })
 
+def piirit(request,kisa_nimi) :
+      return HttpResponse(kisa_nimi + " PIIRIN TULOSTUS" )
+
 #tulosten syotto  
-def syotto(request):
-    manipulator=Syotatulos.AddManipulator()
-    
-#    if request.method == 'POST':
-        
-    data = request.POST.copy()
-    errors = None  
-#   errors = manipulator.get_validation_errors(data)
-    manipulator.do_html2python(data)
-    
-    if not errors:
-           haku = data.clean_data
-           halututKisat = Kisa.objects.filter(kisa__nimi = haku[0])
-           halututSarjat = Sarja.objects.filter(sarja__nimi = haku[1]).filter(kisa = halututKisat)
-           halututTehtavat = tehtava.objects.filter(tehtava__jarjestysnro = haku[2]).filter(sarja = halututSarjat)
-           
-           TulosForm = form_for_model(halututTehtavat)
-           
-           return render_to_response('tupa/syotto.html', {'form': TulosForm})
-        
-    else:
-           return HttpResponse('Ei hjuva')
- #   else:
-  #      return HttpResponse('Ei menty hakuun')
-        
-#    form = FormWrapper(manipulator, {}, {})
-#    return render_to_response('tupa/syotto.html', {'form': form})
-
-
-
-def tulokset(request,sarja_id):
-    sarja = Sarja.objects.filter(id=sarja_id)[0]
-    tulokset= sarja.laskeTulokset()
-    return render_to_response('tupa/tulokset2.html', {'tulos_taulukko' : tulokset }  )
+def syotto(request,sarja_id):
+    request.POST["pisteet"]
+    return render_to_response('tupa/syotto.html', {})
 
--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
-     (r'^tupa/$', 'web.tupa.views.index'),
-     (r'^tupa/(?P<kisa_id>\d+)/$', 'web.tupa.views.kisa'),
-     (r'^tupa/(?P<kisa_id>\d+)/(?P<sarja_id>\d+)/$', 'web.tupa.views.sarja'),
-     (r'^tupa/tulokset/(?P<sarja_id>\d+)/$', 'web.tupa.views.tulokset'),
-     (r'^tupa/syotto/$', 'web.tupa.views.syotto'),
+urlpatterns = patterns('web.tupa.views.',
+     (r'^tupa/$', 'index'),
      (r'^tupa/admin/', include('django.contrib.admin.urls')),
+     (r'^tupa/(?P<kisa_nimi>\w+)/$', 'kisa'),
+     (r'^tupa/(?P<kisa_nimi>\w+)/maarita/$', 'maaritaKisa'),
+     (r'^tupa/(?P<kisa_nimi>\w+)/maarita/tehtava/(?P<tehtava_id>\d+)/$', 'maaritaTehtava'),
+     (r'^tupa/(?P<kisa_nimi>\w+)/syota/$', 'syotaKisa'),
+     (r'^tupa/(?P<kisa_nimi>\w+)/syota/tehtava/(?P<tehtava_id>\d+)/$', 'syotaTehtava'),
+     (r'^tupa/(?P<kisa_nimi>\w+)/tulosta/sarja/(?P<sarja_id>\d+)/$', 'tulostaSarja'),
+     (r'^tupa/(?P<kisa_nimi>\w+)/tulosta/piirit/$', 'piirit'),
 )
+     #(r'^tupa/(?P<kisa_id>\d+)/$', 'web.tupa.views.kisa'),
+     # (r'^tupa/(?P<kisa_id>\d+)/(?P<sarja_id>\d+)/$', 'web.tupa.views.sarja'),
+     #(r'^tupa/tulokset/(?P<sarja_id>\d+)/$', 'web.tupa.views.tulokset'),
+     #(r'^tupa/syotto/$', 'web.tupa.views.syotto'),
