c1689a7bed5377418fb672ebe21c6377d142d778
Frans Korhonen <frans.korhonen@helsinki.fi>
2009-04-13 08:50:36 +0000

    Alphaa pukkaa.

--- web/tupa/views.py
+++ web/tupa/views.py
@@ -1,7 +1,9 @@
 # coding: latin-1
 from models import *
 from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404
 from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 import operator
 from decimal import *
 from django import newforms as forms
@@ -16,14 +18,49 @@ def index(request):
       return render_to_response('tupa/index.html', {'latest_kisa_list': kisat })
 
 def kisa(request,kisa_nimi) :
-      return render_to_response('tupa/kisa.html', { })
+      kisat = Kisa.objects.filter(nimi=kisa_nimi)
+      kisa=None
+      if kisat:
+          kisa = kisat[0]
+      if kisa:  
+          return render_to_response('tupa/kisa.html', {'kisa' : kisa })
+      else :
+          return HttpResponseRedirect(reverse('web.tupa.views.index'))
 
 def tulosta(request,kisa_nimi):
       sarjat = Sarja.objects.filter(kisa__nimi=kisa_nimi)
       return render_to_response('tupa/tulosta.html', {'sarja_list': sarjat })
 
 def maaritaKisa(request, kisa_nimi):
-      return HttpResponse("KISAN " + kisa_nimi + " MÄÄRITYS"  )
+     kisat=Kisa.objects.filter(nimi=kisa_nimi)
+     kisa=None
+     id=None
+     if kisat:
+         kisa=kisat[0]
+         id=kisa.id
+     posti=None
+     if request.method == 'POST':
+          posti=request.POST
+     
+     # Luodaan Kisa formi
+     kisaForm = KisaForm(kisa,post=posti,id=1)
+     kisaForm.save()
+    
+     # Luodaan Sarja formi
+     sarjaFormit=[]
+     
+     if kisaForm.kisa : 
+         kisa=kisaForm.kisa
+     sFormit=luoSarjaFormit(kisa,posti,tyhjia=4)
+     for s in sFormit :
+             s.save()
+             if not s.empty() :
+                 sarjaFormit.append(s)
+     
+     if request.method == 'POST' and kisaForm.kisa:
+         return HttpResponseRedirect(reverse('web.tupa.views.kisa', args=(kisaForm.kisa.nimi,)))
+     else :
+         return render_to_response('tupa/maaritaKisa.html', { 'kisa_form' : kisaForm, 'sarja_formit' : sarjaFormit })
 
 def maaritaValitseTehtava(request,kisa_nimi):
       sarjat = Sarja.objects.filter(kisa__nimi=kisa_nimi)
@@ -49,14 +86,38 @@ def maaritaVartiot(request,kisa_nimi):
          sarjaVartiot.append( (s,vartioFormit) )
       return render_to_response('tupa/maaritaVartiot.html', { 'sarja_vartiot' : sarjaVartiot })
 
+def maaritaUusiTehtava(request, kisa_nimi, sarja_id) :
+        kisa = get_object_or_404(Kisa, nimi=kisa_nimi)
+        sarja = get_object_or_404(Sarja, id=sarja_id)
+        tehtavaForm = TehtavaForm(sarja)
+        tehtava=None
+        maariteFormit = luoMaariteFormit(tyhjia=5)
+        if request.method == 'POST':
+             tehtavaForm = TehtavaForm(sarja,post=request.POST)
+             tehtavaForm.save()
+             
+             if tehtavaForm.tehtava:
+                 maariteFormit = luoMaariteFormit(tehtavaForm.tehtava,request.POST,tyhjia=5)
+                 for m in maariteFormit :
+                     m.save()
+                 return HttpResponseRedirect(reverse('web.tupa.views.maaritaTehtava', 
+                                                args=(kisa_nimi,tehtavaForm.tehtava.id )))
+        
+        return render_to_response('tupa/maarita_tehtava.html', 
+                                            { 'tehtava' : tehtava ,
+                                            'tehtavaForm' : tehtavaForm , 
+                                            'maariteFormit' : maariteFormit })
+
+
 def maaritaTehtava(request, kisa_nimi, tehtava_id):
-    tehtava = Tehtava.objects.filter(id=tehtava_id)[0]
-    tehtavaForm = TehtavaForm(tehtava)
+    tehtava = get_object_or_404(Tehtava, id=tehtava_id)
+
     maaritteet = SyoteMaarite.objects.filter( tehtava=tehtava )
     maariteFormit = []
     posti=None
     if request.method == 'POST':
           posti=request.POST
+    tehtavaForm = TehtavaForm(tehtava=tehtava,post=posti)
     maariteFormit=luoMaariteFormit(tehtava,posti,tyhjia=3)
     mFormit=[]
     for m in maariteFormit :
@@ -64,13 +125,10 @@ def maaritaTehtava(request, kisa_nimi, tehtava_id):
          if not m.empty() :
              mFormit.append(m)
    
-    tehtavaForm = TehtavaForm(tehtava,posti)
-    teht = Tehtava.objects.filter(id=tehtava_id)[0]
     if tehtavaForm.is_valid() :
-       teht.nimi=tehtavaForm.clean_data['nimi']
-       teht.kaava=tehtavaForm.clean_data['kaava']
-       teht.save()
-
+       tehtavaForm.save()
+    if not tehtavaForm.tehtava:
+       return HttpResponseRedirect(reverse('web.tupa.views.maaritaValitseTehtava', args=(kisa_nimi, )))
     return render_to_response('tupa/maarita_tehtava.html', { 'tehtava' : tehtava ,'tehtavaForm' : tehtavaForm , 'maariteFormit' : mFormit })
 
 def syotaKisa(request, kisa_nimi):
@@ -123,13 +181,13 @@ def syotaTehtava(request, kisa_nimi , tehtava_id) :
                'syotteet' : syoteFormit } )
 
 def tulostaSarja(request, kisa_nimi, sarja_id) :
-      sarja = Sarja.objects.filter(id=sarja_id)[0]
+      sarja = Sarja.objects.get(id=sarja_id)
       lokkeri.clearLog()
       tulokset= sarja.laskeTulokset()
       return render_to_response('tupa/tulokset2.html', {'tulos_taulukko' : tulokset }  )
 
 def sarja(request,sarja_id) :
-      sarja= Kisa.objects.filter(id=sarja_id)[0]
+      sarja= Kisa.objects.get(id=sarja_id)
       return render_to_response('tupa/sarja.html', {'sarja_object': sarja })
 
 def piirit(request,kisa_nimi) :
