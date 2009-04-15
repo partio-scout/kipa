3ea3d972eedc72ac8f6047fdd2efec3d9f4e3802
Frans Korhonen <frans.korhonen@helsinki.fi>
2009-04-15 03:25:16 +0000

    Admin sivusto uudemmalla tavalla.

---
+++ web/tupa/admin.py
@@ -0,0 +1,11 @@
+from web.tupa.models import *
+from django.contrib import admin
+
+admin.site.register(Kisa)
+admin.site.register(Sarja)
+admin.site.register(Tehtava)
+admin.site.register(Vartio)
+admin.site.register(Syote)
+admin.site.register(SyoteMaarite)
+admin.site.register(Allergia)
+admin.site.register(TuomarineuvosTulos)
