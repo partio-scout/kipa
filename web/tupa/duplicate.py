d090bb310b970b166aa08c8205f4e36d93b725f2
Frans Korhonen <frans.korhonen@helsinki.fi>
2009-05-12 18:44:15 +0000

    Törmä muutoksia

---
+++ web/tupa/duplicate.py
@@ -0,0 +1,28 @@
+from models import *
+#coding: latin-1
+
+def kopioiTehtava(tehtava,sarjaan,uusiNimi=None) :
+        # Kopioi itse tehtävä:
+        tNimi=tehtava.nimi
+        if uusiNimi:
+                tNimi=uusiNimi
+        uusiTehtava=Tehtava(sarja=sarjaan,nimi=tNimi,kaava=tehtava.kaava,jarjestysnro=tehtava.jarjestysnro)
+        uusiTehtava.save()
+
+        # Kopioi määritteet:
+        maaritteet = tehtava.syotemaarite_set.all()
+        for m in maaritteet:
+                uusim=SyoteMaarite(nimi=m.nimi,tehtava=uusiTehtava,tyyppi=m.tyyppi,kali_vihje=m.kali_vihje )
+                uusim.save()
+        
+        # Kopioi kaavat:
+        kaavat = tehtava.osapistekaava_set.all()
+        for k in kaavat:
+                uusik=OsapisteKaava(nimi=k.nimi,kaava=k.kaava,tehtava=uusiTehtava)
+                uusik.save()
+
+
+
+
+
+
