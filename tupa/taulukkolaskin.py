6725d16df59e6a439f00ba1d0f5034a4692f8f79
Frans Korhonen <frans.korhonen@helsinki.fi>
2011-03-06 11:46:12 +0000

    Ja bugifixiä...

--- tupa/taulukkolaskin.py
+++ tupa/taulukkolaskin.py
@@ -28,10 +28,17 @@ def dictToMathDict(dictionary) :
                 else : new[k]=dictionary[k]
         return new
 
-def laske(lauseke,m={},f={'num':DictDecimal}):
+def laske(lauseke,m={},funktiot={}):
         """
         Laskee lauseen mikäli se on laskettavissa. Käyttää muuttujista loytyvia arvoja, seka funktioita.
         """
+        # Määritetään numeroluokka eli vakiona lasketaan desimaaliluvuilla:
+        f = { "num" : DictDecimal }
+        f.update(funktiot)
+        f.update(pfunktiot)
+        f.update(lfunktiot)
+        f=dictToMathDict(f)
+
         # Poistetaan välilyonnit ja enterit:
         lause = lauseke.replace('\n','')
         lause = lause.replace('\r','')
@@ -40,13 +47,14 @@ def laske(lauseke,m={},f={'num':DictDecimal}):
         lause=re.sub(r"([-][0](?![0-9.]))",r"",lause) 
         # Korvataan funktiot
         # Vakionumerot numeroinstansseiksi:
-        lause=re.sub(r"((?<![^-+*/(),[])\d+([.]\d+)?)",r"num('\g<1>')",lause)
+        lause=re.sub(r"((?<![^-+*/(,[])-?\d+([.]\d+)?)",r"num('\g<1>')",lause)
         # Korvataan muuttujien nimet oikeilla muuttujilla:
         lause=re.sub(r"\.([a-zA-Z_]\w*)(?=\.)",r"['\g<1>']",lause) # .x. -> [x].
         lause=re.sub(r"(?<!\d)\.([a-zA-Z_0-9]+)",r"['\g<1>']",lause)       # .x  -> [x]
         lause=re.sub(r"([a-zA-Z_]\w*(?=[[]))",r"m['\g<1>']",lause) # x[  -> m[x][
         # Korvataan yksinäiset muuttujat (lähinnä funktioita):
-        lause=re.sub(r"([a-zA-Z_]\w*(?![a-zA-Z_0-9.]*?[[']))",r"f['\g<1>']",lause) # x -> m[x]
+        lause=re.sub(r"([a-zA-Z_]\w*(?![a-zA-Z_0-9.(]*?[[']))",r"m['\g<1>']",lause) # x -> m[x]
+        lause=re.sub(r"([a-zA-Z_]\w*(?![a-zA-Z_0-9.]*?[[']))",r"f['\g<1>']",lause) # x( -> f[x](
         tulos=None
         # lasketaan tulos:
         try: 
@@ -70,21 +78,13 @@ def laskeTaulukko(taulukko,muuttujat) :
         """
         Laskee taulukon alkiot jotka ovat laskettavissa, muuttujista loytyvilla muuttujilla seka funktioilla.
         """
-        # Määritetään numeroluokka eli vakiona lasketaan desimaaliluvuilla:
-        f = { "num" : DictDecimal }
-        f.update(pfunktiot)
-        f.update(lfunktiot)
-
         # Päivitetään käyttajän muuttujilla:
-        m = {  } 
-        m.update(muuttujat)
-        m=dictToMathDict(m)
-        f=dictToMathDict(f)
+        m=dictToMathDict(muuttujat)
         tulokset=[]
         for x in taulukko :
                 rivi=[]
                 for lause in x : 
-                        tulos=laske(lause,m,f)
+                        tulos=laske(lause,m)
                         if type(tulos)==Decimal : rivi.append(Decimal(tulos).quantize(Decimal('0.1'),
                                                                         rounding=ROUND_HALF_UP )) 
                         else: rivi.append(tulos)
