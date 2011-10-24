bda5b7eba1951a3aa75c8c20d7fad372b0337ea8
Frans Korhonen <frans.korhonen@helsinki.fi>
2011-10-24 04:46:32 +0000

    Ja log.py file unohtui.

---
+++ web/tupa/log.py
@@ -0,0 +1,56 @@
+
+laskentaloki=""
+
+def __noLogString(string):
+        return None
+
+def __logString(string):
+        string=string.replace("\n","<br>")
+        string=string.replace(" ","&nbsp")
+        global laskentaloki
+        laskentaloki+=string+"<br>"
+
+def __noLogFunction(function,params,result):
+        return None
+
+def __logFunction(function,params,result):
+        global laskentaloki
+        
+        if(params):
+            if hasattr(params, '__len__') and len(params)==1 and (hasattr(params[0], 'keys') or hasattr(params[0], '__contains__' )) and  len(params[0])>5 :
+                laskentaloki+= unicode(function.__name__)  + u"(x)=" + unicode(result) + "  kun <br>"
+                laskentaloki+= "x= " + unicode(params[0]) 
+
+            else :
+                laskentaloki+= unicode(function.__name__)  + "("
+                try :
+                        for p in params : laskentaloki += unicode(p) + ", "
+                except: pass
+                laskentaloki=laskentaloki[:-2]
+                laskentaloki+= ")= " + unicode(result)
+        laskentaloki += "<br>"
+
+logString= __logString
+logFunction= __logFunction
+def enableLogging() :
+        global logString
+        global logFunction
+        logString= __logString
+        logFunction= __logFunction
+
+def disableLogging() :
+        global logString
+        global logFunction
+        logString= __noLogString
+        logFunction= __noLogFunction
+
+def clearLoki() :
+        global laskentaloki
+        laskentaloki=""
+
+def palautaLoki() :
+        global laskentaloki
+        return laskentaloki
+
+disableLogging()
+
