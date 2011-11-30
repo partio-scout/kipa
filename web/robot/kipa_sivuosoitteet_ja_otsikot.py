0eeaea749d31c2424bea679c95a1ff04a794cc5b
Samu Wikstedt <swiksted@gmail.com>
2011-11-30 21:03:19 +0000

    pohja pääsivun linkeille, linkkinimille ja alasivuotsikoille

---
+++ web/robot/kipa_sivuosoitteet_ja_otsikot.py
@@ -0,0 +1,48 @@
+# Kipan sivu
+#
+#Suoritusten syöttö
+
+http://127.0.0.1:8000/kipa/testi_kisa/syota/
+Kipa - Syötä tuloksia
+
+http://127.0.0.1:8000/kipa/testi_kisa/syota/tarkistus/
+Kipa - Syötä tuloksia
+
+#Tulokset
+http://127.0.0.1:8000/kipa/testi_kisa/tulosta/
+Kipa - Tulokset sarjoittain
+
+http://127.0.0.1:8000/kipa/testi_kisa/maarita/tuomarineuvos/
+Kipa - Tuomarineuvoston antamien tulosten määritys
+
+http://127.0.0.1:8000/kipa/testi_kisa/tulosta/tilanne/
+Kipa - Tupa - laskennan tilanne
+
+http://127.0.0.1:8000/kipa/testi_kisa/heijasta/sarja/
+Sarjakohtaiset tulokset
+
+#Kisan Määritykset
+http://127.0.0.1:8000/kipa/testi_kisa/maarita/
+Kipa - Määritä kisa
+
+http://127.0.0.1:8000/kipa/testi_kisa/maarita/vartiot/
+Kipa - Määritä vartiot
+
+http://127.0.0.1:8000/kipa/testi_kisa/maarita/tehtava/
+Kipa - Muokkaa tehtävää
+
+http://127.0.0.1:8000/kipa/testi_kisa/maarita/testitulos/
+Kipa - Testituloksien määritys
+
+#Ylläpito
+http://127.0.0.1:8000/kipa/
+Kipa - kaikki kisat
+
+http://127.0.0.1:8000/kipa/testi_kisa/tallenna/
+????
+
+http://127.0.0.1:8000/kipa/testi_kisa/korvaa/
+Kipa - Korvaa kisa tiedostosta
+
+http://127.0.0.1:8000/kipa/testi_kisa/poista/
+Kipa - Poista kisa
