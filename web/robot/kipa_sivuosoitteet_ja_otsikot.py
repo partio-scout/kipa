c79cbf1991f8c1ab59a7726937113ebd54a578ff
Samu Wikstedt <swiksted@gmail.com>
2011-12-01 18:00:39 +0000

    Lisää testejä ja muuttujafile

--- web/robot/kipa_sivuosoitteet_ja_otsikot.py
+++ web/robot/kipa_sivuosoitteet_ja_otsikot.py
@@ -1,43 +1,48 @@
+# coding=UTF8
+
+# Testikisan nimimääritys, oletusarvona: testikisa
+# TESTIKISA = 'Kisa testausta varten'
+
 # Kipa pääsivu
 
 KIPA_OTSIKKO = 'Kipa - kaikki kisat'
-KIPA_URL = 'http://127.0.0.1:8000/'
+KIPA_URL = 'http://127.0.0.1:8000/kipa'
 
 #Suoritusten syöttö
 
 TULOSTEN_SYOTTO_OTSIKKO = 'Kipa - Syötä tuloksia'
-TULOSTEN_SYOTTO_URL' = syota/'
+TULOSTEN_SYOTTO_URL = 'syota'
 
 TULOSTEN_SYOTTO_TARKISTUS_OTSIKKO = 'Kipa - Syötä tuloksia'
-TULOSTEN_SYOTTO_TARKISTUS_URL = 'syota/tarkistus/'
+TULOSTEN_SYOTTO_TARKISTUS_URL = 'syota/tarkistus'
 
 #Tulokset
 
 TULOSTEN_TARKISTUS_OTSIKKO = 'Kipa - Tulokset sarjoittain'
-TULOSTEN_TARKISTUS_URL = 'tulosta/'
+TULOSTEN_TARKISTUS_URL = 'tulosta'
 
 TUOMARINEUVOSTO_OTSIKKO = 'Kipa - Tuomarineuvoston antamien tulosten määritys'
-TUOMARINEUVOSTO_URL = 'maarita/tuomarineuvos/'
+TUOMARINEUVOSTO_URL = 'maarita/tuomarineuvos'
 
 LASKENNAN_TILANNE_OTSIKKO = 'Kipa - Tupa - laskennan tilanne'
-LASKENNAN_TILANNE_URL = 'tulosta/tilanne/'
+LASKENNAN_TILANNE_URL = 'tulosta/tilanne'
 
 SARJAKOHTAISET_TULOKSET_OTSIKKO = 'Sarjakohtaiset tulokset'
-SARJAKOHTAISET_TULOKSET_URL = 'heijasta/sarja/'
+SARJAKOHTAISET_TULOKSET_URL = 'heijasta/sarja'
 
 #Kisan Määritykset
 
 KISAN_MAARITYS_OTSIKKO = 'Kipa - Määritä kisa'
-KISAN_MAARITYS_URL = 'maarita/'
+KISAN_MAARITYS_URL = 'uusiKisa/maarita'
 
 VARTIOIDEN_MAARITYS_OTSIKKO = 'Kipa - Määritä vartiot'
-VARTIOIDEN_MAARITYS_URL = 'maarita/vartiot/'
+VARTIOIDEN_MAARITYS_URL = 'maarita/vartiot'
 
 TEHTAVAN_MAARITYS_OTSIKKO = 'Kipa - Muokkaa tehtävää'
-TEHTAVAN_MAARITYS_URL = 'maarita/tehtava/'
+TEHTAVAN_MAARITYS_URL = 'maarita/tehtava'
 
 TESTITUOKSIEN_MAARITYS_OTSIKKO = 'Kipa - Testituloksien määritys'
-TESTITUOKSIEN_MAARITYS_URL = 'maarita/testitulos/'
+TESTITUOKSIEN_MAARITYS_URL = 'maarita/testitulos'
 
 #Ylläpito
 
@@ -46,12 +51,13 @@ KAIKKI_KISAT_OTSIKKO = 'Kipa - kaikki kisat'
 KAIKKI_KISAT_URL = KIPA_OTSIKKO
 
 # Tallenna kisa, ei vielä osaamista filen vastaanottoon, TBD
-"http://127.0.0.1:8000/kipa/testi_kisa/tallenna/
+#"http://127.0.0.1:8000/kipa/testi_kisa/tallenna/
 
 # Kisan tuonti tiedostosta, tod.näk helppoa käyttämällä fixtuuria. TBD 
 #http://127.0.0.1:8000/kipa/testi_kisa/korvaa/
 #Kipa - Korvaa kisa tiedostosta
 
+# Posta Kisa
 KISAN_POISTO_OTSIKKO = 'Kipa - Poista kisa'
-KISAN_POISTO_URL = 'poista/'
+KISAN_POISTO_URL = 'poista'
 
