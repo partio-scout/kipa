efd30ad41f50d73f343096f0dda45f4a3f2efb27
Samu Wikstedt <swiksted@gmail.com>
2011-12-02 21:39:17 +0000

    Siirretty testit trunkista leelase 1.5 haaraan

---
+++ web/robot/kipa_sivuosoitteet_ja_otsikot.py
@@ -0,0 +1,63 @@
+# coding=UTF8
+
+# Testikisan nimimääritys, oletusarvona: testikisa
+# TESTIKISA = 'Kisa testausta varten'
+
+# Kipa pääsivu
+
+KIPA_OTSIKKO = 'Kipa - kaikki kisat'
+KIPA_URL = 'http://127.0.0.1:8000/kipa'
+
+#Suoritusten syöttö
+
+TULOSTEN_SYOTTO_OTSIKKO = 'Kipa - Syötä tuloksia'
+TULOSTEN_SYOTTO_URL = 'syota'
+
+TULOSTEN_SYOTTO_TARKISTUS_OTSIKKO = 'Kipa - Syötä tuloksia'
+TULOSTEN_SYOTTO_TARKISTUS_URL = 'syota/tarkistus'
+
+#Tulokset
+
+TULOSTEN_TARKISTUS_OTSIKKO = 'Kipa - Tulokset sarjoittain'
+TULOSTEN_TARKISTUS_URL = 'tulosta'
+
+TUOMARINEUVOSTO_OTSIKKO = 'Kipa - Tuomarineuvoston antamien tulosten määritys'
+TUOMARINEUVOSTO_URL = 'maarita/tuomarineuvos'
+
+LASKENNAN_TILANNE_OTSIKKO = 'Kipa - Tupa - laskennan tilanne'
+LASKENNAN_TILANNE_URL = 'tulosta/tilanne'
+
+SARJAKOHTAISET_TULOKSET_OTSIKKO = 'Sarjakohtaiset tulokset'
+SARJAKOHTAISET_TULOKSET_URL = 'heijasta/sarja'
+
+#Kisan Määritykset
+
+KISAN_MAARITYS_OTSIKKO = 'Kipa - Määritä kisa'
+KISAN_MAARITYS_URL = 'uusiKisa/maarita'
+
+VARTIOIDEN_MAARITYS_OTSIKKO = 'Kipa - Määritä vartiot'
+VARTIOIDEN_MAARITYS_URL = 'maarita/vartiot'
+
+TEHTAVAN_MAARITYS_OTSIKKO = 'Kipa - Muokkaa tehtävää'
+TEHTAVAN_MAARITYS_URL = 'maarita/tehtava'
+
+TESTITULOKSIEN_MAARITYS_OTSIKKO = 'Kipa - Testituloksien määritys'
+TESTITULOKSIEN_MAARITYS_URL = 'maarita/testitulos'
+
+#Ylläpito
+
+# huom Listaa kaikki kisat linkki vie pääsivulle
+KAIKKI_KISAT_OTSIKKO = 'Kipa - kaikki kisat'
+KAIKKI_KISAT_URL = KIPA_OTSIKKO
+
+# Tallenna kisa, ei vielä osaamista filen vastaanottoon, TBD
+#"http://127.0.0.1:8000/kipa/testi_kisa/tallenna/
+
+# Kisan tuonti tiedostosta, tod.näk helppoa käyttämällä fixtuuria.TBD 
+KISAN_TUONTI_URL = 'korvaa'
+KISAN_TUONTI_OTSIKKO = 'Kipa - Korvaa kisa tiedostosta'
+
+# Poista Kisa
+KISAN_POISTO_OTSIKKO = 'Kipa - Poista kisa'
+KISAN_POISTO_URL = 'poista'
+
