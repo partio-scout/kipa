3fb175e430976e48716c7b7c86ea1546d1269d97
Eero Penttilä <eero.penttila@student.oulu.fi>
2017-11-07 11:18:15 +0200

    urlien muokkausta

--- web/urls.py
+++ web/urls.py
@@ -1,17 +1,17 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-        (r'^kipa/',  include('tupa.urls')),
-        (r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+        url(r'^kipa/',  include('tupa.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+]
 
 if settings.DEBUG :
-        urlpatterns += patterns('',
-                (r'^kipamedia/(?P<path>.*)$', 'django.views.static.serve',
-                {'document_root': settings.STATIC_DOC_ROOT}),)
+        urlpatterns += [
+                #url(r'^kipamedia/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_DOC_ROOT}),
+                ]
 
 handler500 = 'tupa.views.raportti_500'
