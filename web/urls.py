735b36e3c0b55e00ef4abb7292c677c4513340d4
Eero Penttilä <eero.penttila@student.oulu.fi>
2017-11-13 15:02:40 +0200

    root sivun uudelleenohjaus

--- web/urls.py
+++ web/urls.py
@@ -4,12 +4,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
 urlpatterns = [
         url(r'^kipa/',  include('tupa.urls')),
         url(r'^admin/', admin.site.urls),
+        url(r'^$', RedirectView.as_view(url='/kipa/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) #toimii vain kehityskäytössä
 
 if settings.DEBUG :
