3767415b8b5744e72681a29e79d498a9da1d1451
Eero Penttilä <eero.penttila@student.oulu.fi>
2017-12-08 17:16:42 +0200

    Poistettu debug-toolbar

--- web/settings_dev.py
+++ web/settings_dev.py
@@ -21,32 +21,15 @@ if not CACHE_TULOKSET :
         TAUSTALASENTA = False
 '''
 INSTALLED_APPS += [
-    'debug_toolbar', #https://django-debug-toolbar.readthedocs.io/en/stable/installation.html
 ]
 
 MIDDLEWARE += [
-    'debug_toolbar.middleware.DebugToolbarMiddleware', #https://django-debug-toolbar.readthedocs.io/en/stable/installation.html
 ]
 
 STATIC_ROOT = None
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "web/media"),
-]
-
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
 ]
 
 INTERNAL_IPS = [
--- web/urls.py
+++ web/urls.py
@@ -15,9 +15,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG :
-    import debug_toolbar
     urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
         ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler500 = 'tupa.views.raportti_500'
