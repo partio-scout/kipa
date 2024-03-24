a9cdc353a4e4088ff16862a7facd3247ee195ea4
Ilmari Marttila <ilmari.marttila@gmail.com>
2024-03-24 23:18:38 +0200

    Remove clearly useless files
    because eg. duplication.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /app/db
+
 export PYTHONPATH="/usr/local/lib/python2.7/site-packages"
 if [ ! -f "/app/db/secret.txt" ]; then
   tr -dc A-Za-z0-9 < /dev/urandom | head -c 40 > /app/db/secret.txt
