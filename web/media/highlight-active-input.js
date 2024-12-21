11eff78408e2bb6bdf4c65efb4adf3117204172c
Ilmari Marttila <ilmari.marttila@gmail.com>
2024-12-21 21:17:31 +0200

    Autoformat JavaScript and css in /kipamedia
    Autoformatting is made with prettier.

--- web/media/highlight-active-input.js
+++ web/media/highlight-active-input.js
@@ -22,44 +22,44 @@ written by Alf Magne Kalleland.
 Alf Magne Kalleland, 2005
 Owner of DHTMLgoodies.com
 	
-************************************************************************************************************/	
-	
+************************************************************************************************************/
+
 var currentlyActiveInputRef = false;
 var currentlyActiveInputClassName = false;
 
-function highlightActiveInput()
-{
-	if(currentlyActiveInputRef){
-		currentlyActiveInputRef.className = currentlyActiveInputClassName;
-	}
-	currentlyActiveInputClassName = this.className;
-	this.className = 'inputHighlighted';
-	currentlyActiveInputRef = this;
-	
-	
+function highlightActiveInput() {
+  if (currentlyActiveInputRef) {
+    currentlyActiveInputRef.className = currentlyActiveInputClassName;
+  }
+  currentlyActiveInputClassName = this.className;
+  this.className = "inputHighlighted";
+  currentlyActiveInputRef = this;
 }
 
-function blurActiveInput()
-{
-	this.className = currentlyActiveInputClassName;
-	
-	
+function blurActiveInput() {
+  this.className = currentlyActiveInputClassName;
 }
 
+function initInputHighlightScript() {
+  var tags = ["INPUT", "TEXTAREA"];
 
-function initInputHighlightScript()
-{
-	var tags = ['INPUT','TEXTAREA'];
-	
-	for(tagCounter=0;tagCounter<tags.length;tagCounter++){
-		var inputs = document.getElementsByTagName(tags[tagCounter]);
-		for(var no=0;no<inputs.length;no++){
-			if(inputs[no].className && inputs[no].className=='doNotHighlightThisInput')continue;
-			
-			if(inputs[no].tagName.toLowerCase()=='textarea' || (inputs[no].tagName.toLowerCase()=='input' && inputs[no].type.toLowerCase()=='text')){
-				inputs[no].onfocus = highlightActiveInput;
-				inputs[no].onblur = blurActiveInput;
-			}
-		}
-	}
+  for (tagCounter = 0; tagCounter < tags.length; tagCounter++) {
+    var inputs = document.getElementsByTagName(tags[tagCounter]);
+    for (var no = 0; no < inputs.length; no++) {
+      if (
+        inputs[no].className &&
+        inputs[no].className == "doNotHighlightThisInput"
+      )
+        continue;
+
+      if (
+        inputs[no].tagName.toLowerCase() == "textarea" ||
+        (inputs[no].tagName.toLowerCase() == "input" &&
+          inputs[no].type.toLowerCase() == "text")
+      ) {
+        inputs[no].onfocus = highlightActiveInput;
+        inputs[no].onblur = blurActiveInput;
+      }
+    }
+  }
 }
